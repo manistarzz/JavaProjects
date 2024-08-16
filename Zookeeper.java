ba7705cb4bd6363b820ba7cdcd4a285046467ef6
Emmauel Gyamfi <116001513+manistarzz@users.noreply.github.com>
2024-08-16 19:07:24 +0100

    Update and rename Main (1).java to Zookeeper.java
    In this project, I created a program that helps the zookeepers check on the animals and make sure that they're doing well. My product will be able to process commands from the zookeepers and display the animals on a monitor.

--- Zookeeper.java
+++ Zookeeper.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Zookeeper {
     public static void main(String[] args) {
         String camel = """
                 Switching on the camera in the camel habitat...
